dced948fdc486f4c221e7431698afb1d7182ce59
Richard Li <richard@datawire.io>
2017-06-29 12:01:22 -0400

    Initial commit

--- ambassador/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-APPDIR=${APPDIR:-/application}
-echo "$APPDIR"
-
-/usr/bin/python3 "$APPDIR/envoy-restarter.py" /etc/envoy-restarter.pid "$APPDIR/envoy-wrapper.sh" &
-/usr/bin/python3 "$APPDIR/ambassador.py" "$APPDIR/envoy-template.json" /etc/envoy.json /etc/envoy-restarter.pid
